643b6f6e08085932d4e2d95ed765fe90360a13f2
yash-mali-001 <140331131+yash-mali-001@users.noreply.github.com>
2024-08-01 02:13:28 +0530

    Updated some files

--- src/main/java/com/beingDev/todoWebApp/security/SpringSecurityConfiguration.java
+++ src/main/java/com/beingDev/todoWebApp/security/SpringSecurityConfiguration.java
@@ -17,15 +17,32 @@ public class SpringSecurityConfiguration {
 
     @Bean
     public InMemoryUserDetailsManager createUserDetailsManager() {
-        Function<String, String> passwordEncoder = input -> passwordEncoder().encode(input);
-
-        UserDetails userDetails = User.builder().passwordEncoder(passwordEncoder).username("beingDev").password("beingDev").roles("USER", "ADMIN").build();
-
+        String username = "Yash";
+        String password = "123456";
+        UserDetails userDetails = createNewUser(username, password);
         return new InMemoryUserDetailsManager(userDetails);
+    }
+
+    public UserDetails createNewUser(String username, String password) {
+        Function<String, String> passwordEncoder = input -> passwordEncoder().encode(input);
+        return User.builder().passwordEncoder(passwordEncoder).username(username).password(password).roles("USER", "ADMIN").build();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+//        httpSecurity.authorizeHttpRequests(authz -> authz
+//                        .anyRequest().authenticated()
+//                )
+//                .formLogin(withDefaults())
+//                .csrf(AbstractHttpConfigurer::disable) // Disable CSRF protection
+//                .headers(headers -> headers
+//                        .frameOptions().disable() // Disable X-Frame-Options
+//                );
+//        return httpSecurity.build();
+//    }
 }
